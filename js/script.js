ef94729dabeb01628b25fa020402a525f13b989f
Joey <joey@joey>
2015-10-21 23:03:55 -0500

    Removed unneeded code from script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,1 @@
-/*
-$( function() {
-	$( window ).on( 'scroll', function() {
-		distanceFromTop = $( window ).scrollTop();
-		
-		if ( distanceFromTop == 0 ) {
-			$( '#site-header' ).removeClass( 'scrolled' );
-		} else {
-			$( '#site-header' ).addClass( 'scrolled' );
-		}
-	} );
-} );
-*/
+//
