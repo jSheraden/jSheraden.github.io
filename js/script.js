09c5fc52291e35a4ae2217ef0bd316ed91c887e5
Joey <joey@joey>
2015-10-21 22:17:37 -0500

    Added stylesheet, as well as js and bower_components folders.

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*
+$( function() {
+	$( window ).on( 'scroll', function() {
+		distanceFromTop = $( window ).scrollTop();
+		
+		if ( distanceFromTop == 0 ) {
+			$( '#site-header' ).removeClass( 'scrolled' );
+		} else {
+			$( '#site-header' ).addClass( 'scrolled' );
+		}
+	} );
+} );
+*/
