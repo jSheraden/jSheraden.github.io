c092393b4b6bd874fcfd3cfa4d0948cb5a62af93
Joseph <josasheraden@gmail.com>
2016-09-01 03:25:35 -0500

    Testing some jquery

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
   $('#copyright').html('&copy ' + new Date().getFullYear());
+  
+  jQuery.event.add(window, 'load', resizeFrame);
+  jQuery.event.add(window, 'resize', resizeFrame);
+  
+  function resizeFrame() {
+    var jumbotronPadding = $(window).width() < 750 ? '100px 0' : '220px 0';
+    $('.jumbotron').style({ 'padding': jumbotronPadding });
+  }
 });
