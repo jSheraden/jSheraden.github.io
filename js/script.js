86ee86242ab393b2bb352b83099e1d32959b4690
Joseph <josasheraden@gmail.com>
2017-03-28 14:15:56 -0500

    Working on parallax scrolling background.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
   $('#copyright').html('&copy ' + new Date().getFullYear());
-  
+
+  $window = $(window);
+
+  $('.parallax').each(function() {
+    var $div = $(this);
+
+    $window.scroll(function() {
+      var yPos = -($window.scrollTop() / $div.data('scrolling-speed'));
+      var bgPos = '50% ' + yPos + 'px';
+    
+      $div.css({ backgroundPosition: bgPos });
+    });
+  });
+
   jQuery.event.add(window, 'load', resizeFrame);
   jQuery.event.add(window, 'resize', resizeFrame);
   
