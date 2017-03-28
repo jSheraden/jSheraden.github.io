f8db3a59c798a91a62e9b31a1f34e9e95552818e
Joseph <josasheraden@gmail.com>
2017-03-28 17:14:01 -0500

    Centered the jumbotrons vertically

--- js/script.js
+++ js/script.js
@@ -14,6 +14,7 @@ $(document).ready(function() {
     });
   });
 
+  /*
   jQuery.event.add(window, 'load', resizeFrame);
   jQuery.event.add(window, 'resize', resizeFrame);
   
@@ -21,4 +22,9 @@ $(document).ready(function() {
     var jumbotronPadding = $(window).width() < 750 ? '100px 0' : '220px 0';
     $('.jumbotron').css({ 'padding': jumbotronPadding });
   }
+  */
+  
+  var jtMargin = ($('.parallax').height() - $('.jumbotron').height()) / 2;
+
+  $('.jumbotron').css({ margin: jtMargin + 'px 0' });
 });
